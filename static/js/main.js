221d719e84c20e2436247d622c1ffea9c8a0e4f8
sabrinadchan <spencerchan@uchicago.edu>
2021-04-11 01:36:11 -0500

    Create properties dictionary

--- static/js/main.js
+++ static/js/main.js
@@ -299,3 +299,14 @@ const races = {
   "Rent Control": {district: "var", race: "Rent Control", elections: ["midp18", "midg18", "mg19"]},
 }
 
+const pctProperties = {
+  "full_text": "f",
+  "renterPct": "r",
+  "whitePct": "w",
+  "blackPct": "b",
+  "latinoPct": "l",
+  "asianPct": "a",
+  "income": "i",
+  "spanish": "s",
+  "non-english": "n",
+}
